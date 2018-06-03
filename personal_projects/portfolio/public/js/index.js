529be3230f3bf46a4b9ace23d30cf2ab0c826358
Luis Gutierrez <spartanxlg@gmail.com>
2018-06-03 12:16:23 -0700

    creating portfolio in react

--- personal_projects/portfolio/public/js/index.js
+++ personal_projects/portfolio/public/js/index.js
@@ -1,17 +1,22 @@
 
 $(document).ready(function() {
-	$("#nav-icon").click(function() {
-		$("#nav-icon").toggle().hide();
+	$("#nav-icon, .closebtn").click(function() {
+		$("#nav-icon").toggleClass("nav-icon_hide");
 	})
+
+	$("#header > h1").delay(500).animate({top:"20px"},2000);
+
+	$(".sticky-container").css("display","none").slideDown(2000);
+
+	$("#nav-icon").css("display","none").slideDown(2000);
+
 })
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
   function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
 }
